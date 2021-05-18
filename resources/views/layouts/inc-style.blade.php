1a96381268b1d7b83620c892a1c19137bb355ea6
kimkundad <kim.kundad@gmail.com>
2021-05-18 20:23:44 +0700

    no message

--- resources/views/layouts/inc-style.blade.php
+++ resources/views/layouts/inc-style.blade.php
@@ -1,6 +1,6 @@
 <!-- CSS
 ================================================== -->
-<link rel="stylesheet" href="{{ url('assets/css/style.css') }}?v2">
+<link rel="stylesheet" href="{{ url('assets/css/style.css') }}?v2.5">
 <link rel="stylesheet" href="{{ url('assets/css/colors/main.css') }}" id="colors">
 <link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
 <link rel="shortcut icon" href="{{ url('img/favicon.png') }}" />
